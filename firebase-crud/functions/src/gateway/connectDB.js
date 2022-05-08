0856b06724dd1c5a84ce3d8148ff776b904237e0
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2022-05-08 00:16:26 -0400

    Delete firebase-crud directory

--- firebase-crud/functions/src/gateway/connectDB.js
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-
-import serviceAccount from "../../../credentials"
-
-admin.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-});
-
-export const db = admin.firestore();
